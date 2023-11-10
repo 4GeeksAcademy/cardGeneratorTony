9969e7c326ece47ce4117ca0ac36e1b4028d221d
Tony1406 <142957069+Tony1406@users.noreply.github.com>
2023-11-10 23:06:35 +0000

    Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let numeros = ['2', '3', '4', '5', '6', '7', '8', 'J', 'Q', 'K', 'A'];
+let iconos = ['♣️', '♥️', '♠️', '♦️'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+window.onload = function cambiarLetra() { 
+    let cambiarNum = numeros[Math.floor(Math.random() * numeros.length)];
+    document.querySelector('#letra').innerText = cambiarNum;
+    cambiarIcono();
 };
+
+function cambiarIcono() { 
+    let cambiarIco = iconos[Math.floor(Math.random() * iconos.length)];
+    document.querySelector('#icono1').innerText = cambiarIco;
+    document.querySelector('#icono2').innerText = cambiarIco;
+    if (cambiarIco == '♥️' || cambiarIco == '♦️'){
+        document.getElementById("icono1").style.color = "red";
+        document.getElementById("icono2").style.color = "red";
+    }; 
+};
+
